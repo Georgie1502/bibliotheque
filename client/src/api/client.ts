1b69d6c5be87b870a972cf5b1733a74fcaa5f7f0
Eric-Philippe <ericphlpp@gmail.com>
2026-03-25 09:24:31 +0100

    Prod URL

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -24,7 +24,7 @@ export {
 } from "./errors";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+  import.meta.env.VITE_API_BASE_URL || "https://coofb.homeserver-ericp.fr";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
